8642ca500ffb5c43ac93e5b05e86323fb235e38c
dvertegaal <doris.vertegaal@deltares.nl>
2024-10-15 09:30:06 +0200

    final fiat example

--- examples/fiat_hydromt.py
+++ examples/fiat_hydromt.py
@@ -9,25 +9,24 @@ import matplotlib.pyplot as plt
 import shutil
 
 #%%
+# Define output folder and model name
 model_root = 'FIAT_database'  # where to save the FIAT model
-model_name = "example" # name of the case study
+model_name = "example_local" # name of the case study
 
 #%%
+# Set up data catalog and hydrmot logger
 model_folder = Path(model_root) / model_name  # full path to model folder
 data_catalog = Path(os.path.abspath("")) / "data" / "hydromt_fiat_catalog_global.yml"  # path to data catalog relative to this notebook
 logger_name = "hydromt_fiat"  # name of the logger
 logger = setuplog(logger_name, log_level=10) # setup logger
 
 #%%
-# domain_fn = Path('./data/KingstonUponHull_domain.geojson')
-# region = gpd.read_file(domain_fn)
-# flood_area = gpd.read_file("c:/Code/COMPASS/sfincs_models/sfincs_MZ_ERA5/gis/floodmap.tif")
-region = gpd.read_file("c:/Code/COMPASS/COMPASS/sfincs_sofala/sfincs_sofala/gis/region.geojson")
+# Read in the region of interest
+region = gpd.read_file(r"c:\Code\Delft-FIAT\hydromt_fiat\examples\FIAT_database\example_bySarah\geoms\region.geojson")
 continent = "Africa"
 country = "Mozambique"
-# continent = 'Europe'
-# country = 'United Kingdom'
 
+# Uncomment to create a region from lat lon coordinates
 # area_of_interest = {
 #     "type": "FeatureCollection",
 #     "features": [
@@ -52,13 +51,6 @@ country = "Mozambique"
 # region = gpd.GeoDataFrame.from_features(area_of_interest, crs="EPSG:4326")
 
 #%%
-from pyproj import CRS
-# Specify the CRS for the region (for example, UTM zone 36S)
-utm_crs = CRS.from_epsg(32736)  # Replace with the correct EPSG code
-
-# Ensure the region geometry is set to the correct CRS
-region = region.to_crs(utm_crs)
-
 # Save the GeoDataFrame as GeoJSON
 region.to_file("region.geojson", driver='GeoJSON')
 
@@ -66,7 +58,7 @@ region.to_file("region.geojson", driver='GeoJSON')
 ### Setup vulnerability parameters ###
 vulnerability_fn = "jrc_vulnerability_curves"
 vulnerability_identifiers_and_linking_fn = "jrc_vulnerability_curves_linking"
-unit = "m"
+unit = "meters"
 
 ### Setup exposure parameters ###
 asset_locations = "OSM"
@@ -74,7 +66,15 @@ occupancy_type = "OSM"
 max_potential_damage = "jrc_damage_values"
 ground_floor_height = 0
 damage_types = ["total"]
-unit = "m"
+unit = "meters"
+crs = "EPSG:4326"
+
+# Set up hazard parameters
+path_map = "c:/Code/COMPASS/sfincs_models/sfincs_MZ_ERA5/gis/floodmap.tif"
+crs_flood = "EPSG:32736"
+map_type = "max_depth"
+# not sure about vertical ref but 
+# the default "datum" does not give different result than "DEM"
 
 ### Setup output parameters ###
 output_dir = "output"
@@ -82,6 +82,7 @@ output_csv_name = "output.csv"
 output_vector_name = "spatial.gpkg"
 
 #%%
+# Define model configurations
 configuration = {
     "setup_output": {
         "output_dir": output_dir,
@@ -100,33 +101,34 @@ configuration = {
         "max_potential_damage": max_potential_damage,
         "ground_floor_height": ground_floor_height,
         "unit": unit,
+        "dst_crs": crs,
         "damage_types": damage_types,
         "country": country,
+    },
+    "setup_hazard": {
+        "map_fn": path_map,
+        "map_type": map_type,
+        "crs": crs_flood, 
+    },
+    "setup_global_settings": {
+        "crs": 4326,
     },
 }
 
 #%%
+# Set up model
 if model_folder.exists():
     shutil.rmtree(model_folder)
 fiat_model = FiatModel(root=model_folder, mode="w", data_libs=[data_catalog], logger=logger)
 
 #%%
+# Build the model
 fiat_model.build(region={"geom": region}, opt=configuration, write=False)
 
 #%%
 # Get the geodataframe with exposure data
 # gdf = fiat_model.exposure.get_full_gdf(fiat_model.exposure.exposure_db)
 # gdf.to_file('exposure_test.geojson', driver='GeoJSON')
-
-#%%
-# gdf[['Secondary Object Type', 'geometry']]
-# Plot the region and the secondary object types of the exposure data
-# m = region.explore(name='Region', style_kwds={'color': 'black', 'fill': False})
-# m = gdf.explore(m=m, column='Secondary Object Type', name='Exposure types')
-# m
-
-#%%
-# gdf.explore(column='Max Potential Damage: Total')
 
 #%%
 # Get the range of (possible) water depths
@@ -142,10 +144,11 @@ plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
 plt.show()
 
 #%%
+# Write the model
 fiat_model.write()
 
 #%%
 fiat_model_new = FiatModel(root=model_folder, mode="r", data_libs=[data_catalog], logger=logger)
 fiat_model_new.read()
 # %%
-# TO DO: add hazard automatically to setting file by specifying floodmap file
+# To run the model, use the "execute_fiat_example.ipynb" script
